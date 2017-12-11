643c613a2c9caf9c70ac8b2792e513538e2da47b
Afreen Patel <afreens.patel@gmail.com>
2017-12-10 17:15:01 -0800

    Updated search result display table

--- web-app/js/displaySearchResult.js
+++ web-app/js/displaySearchResult.js
@@ -8,6 +8,15 @@ $(function(){
 
 	for(var i=0;i<5;i++)
 	{
-		$("#search_result").append(" <form class='form-horizontal'> <div class='form-group'> <table style='width:50%''> <tr> <th>Source:</th><th>Destination: </th><th>No Of Passenger:</th></tr><tr><td>A</td><td>Z</td><td>1000</td></tr><tr><th>Departure Time:</th><th>Arrival Time: </th><th>Date: </th></tr><tr><td>9:00am</td><td>9:30am</td><td>05/12/17</td></tr><tr><th>Price:</th><th>Total Time: </th><th>Type: </th></tr><tr><td>$3</td><td>11 minute</td><td>SB</td></tr></table></div><div class='form-group'><button type='button' id='book_btn' style='color:red'>Book</button></div></form>");
+		if(i==0)
+		{
+			$("#search_result").append("<form class='form-horizontal'> <div class='form-group' style='border :1px solid black'> <table style='width:100%''> <tr> <th>Source:</th><th>Destination: </th><th>No Of Passenger:</th> <th>Departure Time:</th><th>Arrival Time: </th></tr><tr><td>A</td><td>Z</td><td>1000</td><td>9:00am</td><td>9:30am</td></tr><tr><th>Date: </th> <th>Price:</th><th>Total Time: </th><th>Type: </th></tr><tr><td>05/12/17</td><td>$3</td><td>11 minute</td><td>SB</td><td><button type='button' id='book_btn' style='color:red'>Book</button></td></tr></table></div></form>");
+
+		}
+		else
+		{
+			$("#search_result").append("<form class='form-horizontal'><div class='form-group' style='border :1px solid black'><table style='width:100%'' border ='0'><tr><th>Source:</th><th>Destination: </th><th>No Of Passenger:</th><th>Departure Time:</th><th>Arrival Time: </th></tr><tr><td>A</td><td>Z</td><td>1000</td><td>9:00am</td><td>9:30am</td></tr><tr><th>Date: </th><th>Price:</th><th>Total Time: </th><th>Type: </th><th></th><th></th></tr><tr><td>05/12/17</td><td>$3</td><td>11 minute</td><td>SB</td></tr></table><div style='border-top-style: dashed'><table style='width:100%' border ='0'><tr><th>Source:</th><th>Destination: </th><th>No Of Passenger:</th><th>Departure Time:</th><th>Arrival Time: </th></tr><tr><td>A</td><td>Z</td><td>1000</td><td>9:00am</td><td>9:30am</td></tr><tr><th>Date: </th><th>Price:</th><th>Total Time: </th><th>Type: </th><th></th><th></th></tr><tr><td>05/12/17</td><td>$3</td><td>11 minute</td><td>SB</td><td><button type='button' id='book_btn' style='color:red'>Book</button></td></tr></table> </div></div></form>");
+
+		}
 	}
 });
